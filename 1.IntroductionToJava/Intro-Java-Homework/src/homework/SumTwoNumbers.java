a559bd897fd81e2591cb49a97a059969f6f6844e
ivayloivanof <ivaylo.ivanof@gmail.com>
2015-08-25 00:02:16 +0300

    add SumTwoNumbers

---
+++ 1.IntroductionToJava/Intro-Java-Homework/src/homework/SumTwoNumbers.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class SumTwoNumbers {
+	
+	public static void main(String[] args) {
+		PrintSum(5, 3);
+		PrintSum(12, -7);
+	}
+	
+	public static void PrintSum(int a, int b) {
+		System.out.println(a + b);
+	}
+}
