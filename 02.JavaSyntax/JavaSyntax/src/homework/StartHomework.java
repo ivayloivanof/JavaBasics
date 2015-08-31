f925884b392d44c7c40810eff2d4b902840c45e7
ivayloivanof <ivaylo.ivanof@gmail.com>
2015-08-31 22:57:35 +0300

    update StartHomework

--- 02.JavaSyntax/JavaSyntax/src/homework/StartHomework.java
+++ 02.JavaSyntax/JavaSyntax/src/homework/StartHomework.java
@@ -2,11 +2,13 @@ package homework;
 
 import java.util.Scanner;
 
+import com.sun.javafx.fxml.expression.BinaryExpression;
+
 public class StartHomework {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+/*
 		System.out.println("Calculating rectangle area. Please input 2 numbers:");
 		int a = scanner.nextInt();
 		int b = scanner.nextInt();
@@ -32,6 +34,28 @@ public class StartHomework {
 		double smallest = SmallestOfThreeNumbers.smallestOfThreeNumbers(first, second, third);
 		
 		System.out.println(smallest);
+		
+		System.out.println("Convert decimal to hexadecimal number. Input decimal number:");
+		int decimal = scanner.nextInt();
+		String hex = DecimalToHexadecimal.decimalToHexadecimal(decimal);
+		System.out.println(hex);
+		
+		int numberA = scanner.nextInt();
+		while (numberA < 0 || numberA > 500) {
+			numberA = scanner.nextInt();
+		}
+		
+		float numberB = scanner.nextFloat();
+		float numberC = scanner.nextFloat();
+		int bits = Float.floatToIntBits(numberB);
+				
+		System.out.printf("|%s|%s|%.2f|%.3f|", Integer.toHexString(numberA), Integer.toBinaryString(bits), numberB, numberC);
+		
+		
+		int numberForCountBits = scanner.nextInt();
+		int count = CountOfBitsOne.Count(numberForCountBits);
+		System.out.println(count);
+		*/
 		
 		scanner.close();
 	}
