dc5e0cc0063ae6394f79dbed7d8a8e869f5bc3a4
ivayloivanof <ivaylo.ivanof@gmail.com>
2015-09-08 23:14:08 +0300

    decide first problem from homework

---
+++ 06.JavaCollection/Homework/src/homework/SortArrayOfNumbers.java
@@ -0,0 +1,20 @@
+package homework;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class SortArrayOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        for (int i = 0; i < number; i++) {
+            numbers.add(scanner.nextInt());
+        }
+
+        Collections.sort(numbers);
+        System.out.println(numbers);
+    }
+}
